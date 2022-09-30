533e910c09272891783e46e90dd26e99d27681bb
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-09-30 15:19:18 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
   });
 
   return (
-    <div>
+    <div>                                                                                                                                                        
       <NavBar />
       <Switch>
         <Route path="/movies">
